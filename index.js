5b4e5c067ce0655acec67d4a35f3cc13648b9aa5
shivamrj1035 <srjtheinfinity1035@gmail.com>
2025-01-28 13:55:42 -0500

    Config

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/db.js";
+import userRoute from './routes/user.route.js';
+import cookieParser from 'cookie-parser'
+import cors from 'cors';
+
+dotenv.config({});
+
+// Call function for DB Connection
+connectDB();
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true,
+}));
+
+
+app.use('/api/v1/user', userRoute)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
